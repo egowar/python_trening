c333fc5f1b4992b68d7c7b84cc5c7e3b20f5db5b
egowar <igorvoina@gmail.com>
2020-05-05 12:32:22 +0300

    geometry 2d

---
+++ geom2d/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "igor"
+
+from geom2d.point import *
--- percents.py
+++ percents.py
@@ -1,7 +1,17 @@
 __author__ = "igor"
 
+
 def percents(x, y):
     """what percentage of x is y"""
-    one_percent = x/100
-    result =y/one_percent
-    print(str(y) + " is " + str(result) + " percents of " + str(x))
+    one_percent = x / 100
+    result = y / one_percent
+    # print(str(y) + " is " + str(result) + " percents of " + str(x))
+    return result
+
+
+def print_percents(x, y):
+    """print what percents of x is y"""
+    print(str(y) + " is " + str(percents(x, y)) + " percents of " + str(x))
+
+print_percents(300,30)
+
